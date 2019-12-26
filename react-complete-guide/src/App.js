6c22d2bcf61d486fc2301b173bae9bc51da6a1b5
Bhaavani <vani.uideveloper@gmail.com>
2019-12-26 15:48:48 -0500

    Using children props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>Thsi is testing!</p>
-      <Person name="Roni" age="27"/>
-      <Person name="Tommy" age="4" />
-      <Person name="Iyans" age="22"/>
+      <Person name="Roni" age="27" />
+      <Person name="Tommy" age="4">
+        My Hobbies: Music{" "}
+      </Person>
+      <Person name="Iyans" age="22" />
     </div>
   );
-  
 }
 
 export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
 
-
-const person = (props) => {
-return <p>I'm {props.name} and i am {props.age} years old</p>
+const person = props => {
+  return (
+    <div>
+      <p>
+        I'm {props.name} and i am {props.age} years old
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 export default person;
