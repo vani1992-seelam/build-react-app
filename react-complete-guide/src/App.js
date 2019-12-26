58974b1191ed8d2a52b2ec11db3f4a8bc3772c38
Bhaavani <vani.uideveloper@gmail.com>
2019-12-26 15:19:10 -0500

    my first react app

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>Thsi is testing!</p>
+      <Person />
+    </div>
+  );
+  
+}
+
+export default App;
