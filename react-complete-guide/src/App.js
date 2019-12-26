9a789e2498d78a6963f24048457aaaab47f33aa5
Bhaavani <vani.uideveloper@gmail.com>
2019-12-26 16:13:17 -0500

    using state property

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>Thsi is testing!</p>
-      <Person name="Roni" age="27" />
-      <Person name="Tommy" age="4">
-        My Hobbies: Music{" "}
-      </Person>
-      <Person name="Iyans" age="22" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    Persons: [
+      { name: "Roni", age: 27 },
+      { name: "Tommy", age: 4 },
+      { name: "Ivans", age: 28 }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>Thsi is testing!</p>
+        <button>Switch Names</button>
+        <Person
+          name={this.state.Persons[0].name}
+          age={this.state.Persons[0].age}
+        />
+        <Person
+          name={this.state.Persons[1].name}
+          age={this.state.Persons[1].age}
+        >
+          My Hobbies: Music{" "}
+        </Person>
+        <Person
+          name={this.state.Persons[2].name}
+          age={this.state.Persons[2].age}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
