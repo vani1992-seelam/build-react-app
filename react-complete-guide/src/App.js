a671e5be7a728d0fe35c4121d9efdfac1d216292
Bhaavani <vani.uideveloper@gmail.com>
2019-12-26 18:15:28 -0500

    passing methods/binding between components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,50 +1,56 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const App = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     Persons: [
       { name: "Roni", age: 27 },
       { name: "Tommy", age: 4 },
       { name: "Ivans", age: 28 }
-    ]
-  });
-  const [otherState, setOtherState] = useState("some other value");
-  console.log(personsState, otherState);
+    ],
+    otherState: "some other value"
+  };
 
-  const switchNameHandler = () => {
+  switchNameHandler = newName => {
     //console.log("Clicked!!!");
-    setPersonsState({
+    this.setState({
       Persons: [
-        { name: "Roni seg", age: 27 },
+        { name: newName, age: 27 },
         { name: "Tommy", age: 4 },
         { name: "Ivans", age: 43 }
       ]
     });
   };
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>Thsi is testing!</p>
-      <button onClick={switchNameHandler}>Switch Names</button>
-      <Person
-        name={personsState.Persons[0].name}
-        age={personsState.Persons[0].age}
-      />
-      <Person
-        name={personsState.Persons[1].name}
-        age={personsState.Persons[1].age}
-      >
-        My Hobbies: Music{" "}
-      </Person>
-      <Person
-        name={personsState.Persons[2].name}
-        age={personsState.Persons[2].age}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>Thsi is testing!</p>
+        {/* alternate arrow function for binding  */}
+        <button onClick={() => this.switchNameHandler("Maxiwill")}>
+          Switch Names
+        </button>
+        <Person
+          name={this.state.Persons[0].name}
+          age={this.state.Persons[0].age}
+        />
+        <Person
+          name={this.state.Persons[1].name}
+          age={this.state.Persons[1].age}
+          // passing methos refrences between components
+          click={this.switchNameHandler.bind(this, "Maxwill2")}
+        >
+          My Hobbies: Music{" "}
+        </Person>
+        <Person
+          name={this.state.Persons[2].name}
+          age={this.state.Persons[2].age}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
