6a7e8779b330c9bcb9f4a5e926ea5699501510df
Bhaavani <vani.uideveloper@gmail.com>
2019-12-26 15:29:32 -0500

    Outputting Dynamic Content

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const person = () => {
-    return <p>I'm a Person!</p>
+return <p>I'm a Person and i am {Math.floor(Math.random() * 20)} years old</p>
 };
 
 export default person;
