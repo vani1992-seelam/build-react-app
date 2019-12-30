291cbc1549f2d38c839484fa578c1477be4d4496
Bhaavani <vani.uideveloper@gmail.com>
2019-12-29 20:07:29 -0500

    css modules and media querys

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,14 +1,9 @@
 import React from "react";
-import "./Person.css";
+import classes from "./Person.css";
 
 const person = props => {
-  // const style = {
-  //   "@media (min-width: 500px)": {
-  //     width: "500px"
-  //   }
-  //};
   return (
-    <div className="Person">
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm {props.name} and i am {props.age} years old
       </p>
