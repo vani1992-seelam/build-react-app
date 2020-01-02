66c3c2f0b0bed0c5d279ba251d3d30ed0d104ed9
Bhaavani <vani.uideveloper@gmail.com>
2020-01-02 18:52:11 -0500

    Component Creation Lifecycle in Action

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -16,7 +16,7 @@ const cockpit = props => {
 
   return (
     <div className={classes.Cockpit}>
-      <h1>Hi, I'm a React App</h1>
+      <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}>This is testing!</p>
       <button className={btnClass} onClick={props.clicked}>
         Toggle Persons
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -4,6 +4,10 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js ] Constructor");
+  }
   state = {
     persons: [
       { id: "abcd1", name: "Roni", age: 27 },
@@ -13,6 +17,19 @@ class App extends Component {
     otherState: "some other value",
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js ] getDerivedStateFromProps", props);
+    return state;
+  }
+
+  componentWillMount() {
+    console.log("[App.js ] componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("[App.js ] componentDidMount");
+  }
 
   switchNameHandler = newName => {
     //console.log("Clicked!!!");
@@ -53,6 +70,7 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js ] render");
     let persons = null;
     if (this.state.showPersons) {
       persons = (
@@ -67,6 +85,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
