84062fca03b08d2bef3ec97453cc284db799f7b4
Bhaavani <vani.uideveloper@gmail.com>
2020-01-04 18:53:49 -0500

    useContext react hook for functional components

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useContext } from "react";
 import classes from "./Cockpit.css";
 import AuthContext from "../../context/auth-context";
 
 const cockpit = props => {
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+  console.log(authContext.authenticated);
   useEffect(() => {
     console.log("[cockpit.js] useEffect");
     //HTTP request.....
@@ -46,9 +48,7 @@ const cockpit = props => {
       <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
-      <AuthContext.Consumer>
-        {context => <button onClick={context.login}>Log IN</button>}
-      </AuthContext.Consumer>
+      <button onClick={authContext.login}>Log IN</button>
     </div>
   );
 };
