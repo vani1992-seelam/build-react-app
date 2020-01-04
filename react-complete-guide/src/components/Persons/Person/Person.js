fa782109bcd485862655167b132b8c51756ef082
Bhaavani <vani.uideveloper@gmail.com>
2020-01-03 23:20:32 -0500

    Rendering Adjacent jax elements

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
+import Aux from "../../../hoc/Aux";
 import classes from "./Person.css";
 
 class Person extends Component {
   render() {
     console.log("[Person.js ] rendering ");
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and i am {this.props.age} years old
         </p>
-        <p>{this.props.children}</p>
+        <p key="i2">{this.props.children}</p>
         <input
+          key="i2"
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Aux>
     );
   }
 }
